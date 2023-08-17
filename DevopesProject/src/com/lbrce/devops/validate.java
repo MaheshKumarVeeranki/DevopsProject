460986c464458ea869471ed9decd109c7973598d
AdmiN <AdmiN@ITWS60>
2023-08-17 15:43:48 +0530

    Java Program to Generate Multiplication Table

---
+++ DevopesProject/src/com/lbrce/devops/validate.java
@@ -0,0 +1,16 @@
+package com.lbrce.devops;
+
+public class validate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+		
+
+	}
+
+}
